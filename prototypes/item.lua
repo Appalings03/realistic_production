975345b78ad1ea553c57e365751c56e2d9373d52
Appalings03 <martin.biname@gmail.com>
2025-01-30 20:46:15 +0100

    saving before no batterry

--- prototypes/item.lua
+++ prototypes/item.lua
@@ -1,13 +1,9 @@
-require("")
-require("")
-require("")
-require("")
-require("")
-require("")
-require("")
-require("")
-require("")
-require("")
+require("ore.base-ore.__")
+require("ore.refined-ore.__")
+require("material.__")
+require("machin.__")
+require("advanced_item.__")
+
 
 
 data:extend({
