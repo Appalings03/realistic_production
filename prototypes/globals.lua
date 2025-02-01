0e043c4820cae02ce9ef711786881366f888ab69
Appalings03 <martin.biname@gmail.com>
2025-02-01 19:28:01 +0100

    Finishing Globals Variables and exports for actinium ore

--- prototypes/globals.lua
+++ prototypes/globals.lua
@@ -1,4 +1,16 @@
-local icon = {
-    path = "__realistic_production-main__/graphics/icons/"
+local graphics = {
+    icons = "__realistic_production-main__/graphics/icons/",
+
 }
 
+local size = {
+    ore_stack_size = 50,
+    icon_size = 32,
+    components_stack_size = 100,
+    advanced_components_size = 200,
+    machine_stack_size = 50,
+}
+
+local name ={
+    
+}
--- prototypes/ore/refined-ore/actinium.lua
+++ prototypes/ore/refined-ore/actinium.lua
@@ -3,7 +3,11 @@ require("__prototypes__/globals")
 local actinium_ore = {
     type = "item",
     name = "actiniumore",
-    icon = globals.icon.path + "actinium_ore.png",
+    icon = globals.graphics.path.."actinium_ore.png",
     icon_size = 32,
     subgroup = "raw-ressource",
 }
+
+data:extend({
+    actinium_ore,
+})    
