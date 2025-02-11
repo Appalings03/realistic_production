3f4a0c87befca6ae42c18bc69b92c20fa0758195
Appalings03 <martin.biname@gmail.com>
2025-02-11 09:58:43 +0100

    Adding all the refined elements code, But the name need some change

--- prototypes/ore/refined-ore/actinium.lua
+++ prototypes/ore/refined-ore/actinium.lua
@@ -1,14 +1,56 @@
 -- actinium.lua created automatically
-require("__prototypes__/globals")
-local actinium_ore = {
+require("__realistic_production__/prototypes/globals")
+
+local actinium_ore_item = {
     type = "item",
-    name = "actiniumore",
+    name = "actinium-ore",
     icon = globals.graphics.path.."actinium_ore.png",
     icon_size = 32,
     subgroup = "raw-ressource",
-    
+    order ="c[actinium-ore]",
+    place_results = "actinium-ore",
+    stack_size = 50
+}
+
+local actinium_ore_recipe = {
+    type = "recipe",
+    name = "actinium-ore",
+    enabled = false,
+    category = "crafting",
+    energy_required = 10,
+    ingredients = {
+        {"", 5},
+        {"", 5},
+    },
+    result = "actinium-ore",
+}
+
+local actinium_ore_tech ={
+    type = "technology",
+    name = "actinium-ore-seperator",
+    icon = globals.graphics.path.."actinium_ore.png",
+    icon_size = 128,
+    prerequisites = {""},
+    unit = {
+        count = 150,
+        ingredients = {
+            {"science-pack-1", 1},
+            {"science-pack-2", 1},
+            {"science-pack-3", 1}
+        },
+        time = 30
+    },
+    effects = {
+        {
+            type = "unlock-recipe",
+            recipe = "actinium-ore",
+        },
+        order = "c-c-a"
+    }
 }
 
 data:extend({
-    actinium_ore,
+    actinium_ore_item,
+    actinium_ore_recipe,
+    actinium_ore_tech,
 })    
--- prototypes/ore/refined-ore/polonium.lua
+++ prototypes/ore/refined-ore/polonium.lua
@@ -1,1 +1,55 @@
 -- polonium.lua created automatically
+local ex_item = {
+    type = "item",
+    name = "b",
+    icon = "__modname__/graphics/icons/ex.png",
+    icon_size = 32,
+    subgroup = "raw-resources",
+    order = "c[ex]",
+    place_result = "ex",
+    stack_size = 50
+}
+
+-- Blast Furnace Recipe Prototype
+local ex_recipe = {
+    type = "recipe",
+    name = "ex",
+    enabled = false,
+    energy_required = 10,
+    ingredients = {
+        {"steel-plate", },
+        {"stone-brick", },
+        {"advanced-circuit", }
+    },
+    result = "ex"
+}
+
+-- Blast Furnace Technology Prototype
+local ex_tech = {
+    type = "technology",
+    name = "ex",
+    icon = "__modname__/graphics/technology/ex.png",
+    icon_size = 128,
+    prerequisites = {""},
+    unit = {
+        count = 150,
+        ingredients = {
+            {"science-pack-1", 1},
+            {"science-pack-2", 1},
+            {"science-pack-3", 1}
+        },
+        time = 30
+    },
+    effects = {
+        {
+            type = "unlock-recipe",
+            recipe = "ex"
+        }
+    },
+    order = "c-c-a"
+}
+data:extend({
+    ex_item,
+    ex_recipe,
+    ex_tech,
+})
